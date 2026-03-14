8612d6bd73eee7184063b1aa33c9bfdac05ed346
yyyyystu <lilyoink123@gmail.com>
2026-03-14 12:40:23 -0700

    initial commit

---
+++ dev-00-0.2/src/main/java/cs151/application/Main.java
@@ -0,0 +1,27 @@
+package cs151.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch();
+    }
+
+    private static void launch()
+    {
+
+    }
+}
